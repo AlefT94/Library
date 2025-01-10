5472df6c07e3a18e9d575de8fd8af7c252ff4b26
AlefT94 <alef_teodoro1@hotmail.com>
2025-01-10 14:52:03 -0300

    feat: BookRepository implementation

---
+++ Library.Infrastructure/Persistence/Repositories/BookRepository.cs
@@ -0,0 +1,45 @@
+﻿using Library.Core.Models;
+using Library.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.ConstrainedExecution;
+
+namespace Library.Infrastructure.Persistence.Repositories;
+public class BookRepository(LibraryDbContext dbContext) : IBookRepository
+{
+    public async Task<Book> CreateAsync(Book newBook)
+    {
+        await dbContext.Books.AddAsync(newBook);
+        await dbContext.SaveChangesAsync();
+        return newBook;
+    }
+
+    public async Task<bool> DeleteByIdAsync(int id)
+    {
+        var dbBook = await dbContext.Books.FindAsync(id);
+
+        if (dbBook == null)
+        { 
+            return false;
+        }
+
+        dbBook.SetAsDeleted();
+        await dbContext.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<IEnumerable<Book>> GetAllAsync()
+    {
+        var bookList = await dbContext
+                        .Books
+                        .AsNoTracking()
+                        .ToListAsync();
+
+        return bookList;
+    }
+
+    public async Task<Book>? GetByIdAsync(int id)
+    {
+        var book = await dbContext.Books.FindAsync(id);
+        return book;
+    }
+}
