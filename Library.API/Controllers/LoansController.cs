2acd726bd377a7ccd23a70f4e975503158f1f495
AlefT94 <alef_teodoro1@hotmail.com>
2025-01-15 22:43:19 -0300

    feat: create loan endpoints

---
+++ Library.API/Controllers/LoansController.cs
@@ -0,0 +1,36 @@
+﻿using Library.Application.Loans.Commands.CreateLoan;
+using Library.Application.Loans.Commands.ReturnLoan;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class LoansController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateLoan([FromBody] CreateLoanCommand loan)
+    {
+        var result = await mediator.Send(loan);
+
+        if (!result.isSuccess)
+        {
+            return BadRequest(result.message);
+        }
+
+        return Ok();
+    }
+
+    [HttpPut("return")]
+    public async Task<IActionResult> ReturnLoan([FromBody] ReturnLoanCommand command)
+    {
+        var isSuccess = await mediator.Send(command);
+
+        if (!isSuccess)
+        {
+            return BadRequest();
+        }
+
+        return Ok();
+    }
+}
