e186af50c2df35226f0af5f7da96b908a5a85367
AlefT94 <alef_teodoro1@hotmail.com>
2025-01-15 22:17:37 -0300

    feat: create users repository implementation

---
+++ Library.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Library.Core.Models;
+using Library.Core.Repositories;
+
+namespace Library.Infrastructure.Persistence.Repositories;
+public class UserRepository(LibraryDbContext dbContext) : IUserRepository
+{
+    public async Task<User> CreateAsync(User user)
+    {
+        var newUser = user;
+        await dbContext.AddAsync(newUser);
+        await dbContext.SaveChangesAsync();
+
+        return newUser;
+    }
+
+    public async Task<User> GetByIdAsync(int id) => await dbContext.Users.FindAsync(id);
+}
