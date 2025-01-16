a1562c2eb8653aaf89490394608c6a03556be5c9
AlefT94 <alef_teodoro1@hotmail.com>
2025-01-15 22:31:03 -0300

    feat: create user endpoints

---
+++ Library.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Library.Application.Users.CreateUser;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand user)
+    {
+        var newUser = await mediator.Send(user);
+
+        return Ok(newUser);
+    }
+}
