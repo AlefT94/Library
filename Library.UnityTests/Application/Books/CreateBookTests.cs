8befa6b3438dd50aa77eab301300bdd6782b9dc2
AlefT94 <alef_teodoro1@hotmail.com>
2025-02-02 20:10:23 -0300

    test: add unit  tests to CreateBook and DeleteBook handlers

---
+++ Library.UnityTests/Application/Books/CreateBookTests.cs
@@ -0,0 +1,54 @@
+﻿using Library.Application.Books.Commands.CreateBook;
+using Library.Core.Common;
+using Library.Core.Models;
+using Library.Core.Repositories;
+using NSubstitute;
+
+namespace Library.UnityTests.Application.Books;
+public class CreateBookTests
+{
+    [Fact]
+    public async Task ValidBookData_HandleIsCalled_ShouldReturnSuccessAsync()
+    {
+        // Arrange
+        var respository = Substitute.For<IBookRepository>();
+
+        string title = "Clean Code";
+        string author = "Robert C. Martin";
+        string isbn = "1234567890123";
+        int publishedYear = 2008;
+        var command = new CreateBookCommand(title, author, isbn, publishedYear);
+
+        var handler = new CreateBookHandler(respository);
+
+        // Act
+        var result = await handler.Handle(command, new CancellationToken());
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        //await respository.Received(1).CreateAsync();
+    }
+
+    [Fact]
+    public async Task InvalidBookData_HandleIsCalled_ShouldReturnFailureAsync()
+    {
+        // Arrange
+        var repository = Substitute.For<IBookRepository>();
+
+        string title = "";
+        string author = "Robert C. Martin";
+        string isbn = "1234567890123";
+        int publishedYear = 2008;
+        var command = new CreateBookCommand(title, author, isbn, publishedYear);
+
+        var handler = new CreateBookHandler(repository);
+
+        // Act
+        var result = await handler.Handle(command, new CancellationToken());
+
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.Equal(BooksErrors.TitleRequired.Code, result.Error.Code);
+        await repository.DidNotReceive().CreateAsync(Arg.Any<Book>());
+    }
+}
---
+++ Library.UnityTests/Application/Books/DeleteBookTests.cs
@@ -0,0 +1,55 @@
+﻿using Library.Application.Books.Commands.DeleteBook;
+using Library.Core.Models;
+using Library.Core.Repositories;
+using NSubstitute;
+using System.Net;
+
+namespace Library.UnityTests.Application.Books;
+public class DeleteBookTests
+{
+    [Fact]
+    public async Task BookExists_Delete_Success()
+    {
+        //Arrange
+        string title = "Clean Code";
+        string author = "Robert C. Martin";
+        string isbn = "1234567890123";
+        int publishedYear = 2008;
+        var resultValidBook = Book.Create(title, author, isbn, publishedYear);
+        var validBook = resultValidBook.Value;
+
+        var repository = Substitute.For<IBookRepository>();
+        var handler = new DeleteBookHandler(repository);
+        var command = new DeleteBookCommand(7);
+
+        repository.GetByIdAsync(Arg.Any<int>()).Returns(Task.FromResult((Book?)validBook));
+        repository.DeleteByIdAsync(Arg.Any<int>()).Returns(Task.FromResult(true));
+
+        //Act
+        var result = await handler.Handle(command, new CancellationToken());
+
+        //Assert
+        Assert.True(result);
+        await repository.Received().DeleteByIdAsync(Arg.Any<int>());
+    }
+
+    [Fact]
+    public async Task BookDoesNotExists_Delete_False()
+    {
+        //Arrange
+
+        var repository = Substitute.For<IBookRepository>();
+        var handler = new DeleteBookHandler(repository);
+        var command = new DeleteBookCommand(7);
+
+        repository.GetByIdAsync(Arg.Any<int>()).Returns(Task.FromResult((Book?)null));
+        repository.DeleteByIdAsync(Arg.Any<int>()).Returns(Task.FromResult(true));
+
+        //Act
+        var result = await handler.Handle(command, new CancellationToken());
+
+        //Assert
+        Assert.False(result);
+        await repository.DidNotReceive().DeleteByIdAsync(Arg.Any<int>());
+    }
+}
